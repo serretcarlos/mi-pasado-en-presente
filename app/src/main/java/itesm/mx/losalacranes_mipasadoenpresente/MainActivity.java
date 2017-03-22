81e5df24ecc3791647b3b858ffb21c8b3dff955e
Eduardo Castro <eduardocd1203@gmail.com>
2017-03-21 21:43:48 -0600

    Activities MainActivity y MenuSeleccionRecuerdos
    MainActivity to select user and activity to select the type of memory

--- app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/MainActivity.java
+++ app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/MainActivity.java
@@ -1,13 +1,42 @@
 package itesm.mx.losalacranes_mipasadoenpresente;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton iBtnPersona1 (ImageButton) findViewById(R.id.imageButton_persona1);
+        ImageButton iBtnPersona2 (ImageButton) findViewById(R.id.imageButton_persona2);
+        ImageButton iBtnAgregar (ImageButton) findViewById(R.id.imageButton_agregar);
+        iBtnPersona1.setOnClickListener(this);
+        iBtnPersona2.setOnClickListener(this);
+        iBtnAgregar.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+
+        public void onClick (View v){
+            switch (v.getId()) {
+                case R.id.imageButton_familia:
+                    Intent intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
+                    startActivity(intent);
+                    break;
+                case R.id.imageButton_persona2:
+                    Intent intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
+                    startActivity(intent);
+                    break;
+                case R.id.imageButton_agregar:
+                    Intent intent = new Intent(MainActivity.this, AgregarUsuario.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
     }
 }
