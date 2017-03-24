35b943bcc784d57da69b0c0558389e7513536bcd
serretcarlos <carloserret@gmail.com>
2017-03-24 13:49:47 -0600

    mainactivity errors deletion

--- app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/MainActivity.java
+++ app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/MainActivity.java
@@ -12,31 +12,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton iBtnPersona1 (ImageButton) findViewById(R.id.imageButton_persona1);
-        ImageButton iBtnPersona2 (ImageButton) findViewById(R.id.imageButton_persona2);
-        ImageButton iBtnAgregar (ImageButton) findViewById(R.id.imageButton_agregar);
+        ImageButton iBtnPersona1 =(ImageButton) findViewById(R.id.imageButton_persona1);
+        ImageButton iBtnPersona2  =(ImageButton) findViewById(R.id.imageButton_persona2);
+        ImageButton iBtnAgregar  = (ImageButton) findViewById(R.id.imageButton_agregar);
         iBtnPersona1.setOnClickListener(this);
         iBtnPersona2.setOnClickListener(this);
         iBtnAgregar.setOnClickListener(this);
     }
+
+
+
     @Override
     public void onClick(View v) {
-
-        public void onClick (View v){
-            switch (v.getId()) {
-                case R.id.imageButton_familia:
-                    Intent intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
-                    startActivity(intent);
-                    break;
-                case R.id.imageButton_persona2:
-                    Intent intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
-                    startActivity(intent);
-                    break;
-                case R.id.imageButton_agregar:
-                    Intent intent = new Intent(MainActivity.this, AgregarUsuario.class);
-                    startActivity(intent);
-                    break;
-            }
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.imageButton_familia:
+                intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
+                startActivity(intent);
+                break;
+            case R.id.imageButton_persona2:
+                intent = new Intent(MainActivity.this, MenuSeccionRecuerdos.class);
+                startActivity(intent);
+                break;
+            case R.id.imageButton_agregar:
+                intent = new Intent(MainActivity.this, AgregarUsuario.class);
+                startActivity(intent);
+                break;
         }
     }
 }
