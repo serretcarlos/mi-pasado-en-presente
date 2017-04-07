a441c1e611311b285a6496e4dba3ae2b7623ba63
pablocantu <pabloandres9521@gmail.com>
2017-04-06 22:44:52 -0500

    Pantalla para agregar un usuario nuevo

--- app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/AgregarUsuario.java
+++ app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/AgregarUsuario.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AgregarUsuario extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,8 +12,19 @@ public class AgregarUsuario extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_usuario);
-        Button btnCancelar = (Button) findViewById(R.id.button_cancelar);
-        Button btnGuardar = (Button) findViewById(R.id.button_guardar);
+
+        Button btnCancelar = (Button) findViewById(R.id.button_cancelar_usuario);
+        Button btnGuardar = (Button) findViewById(R.id.button_guardar_usuario);
+        Button btnFoto = (Button) findViewById(R.id.button_foto_usuario);
+
+        EditText etNombre = (EditText) findViewById(R.id.edit_nombre_usuario);
+        EditText etApellid = (EditText) findViewById(R.id.edit_apellido_usuario);
+        EditText etEdad = (EditText) findViewById(R.id.edit_edad_usuario);
+        EditText etLugarNacimiento = (EditText) findViewById(R.id.edit_lugar_nacimiento_usuario);
+        EditText etEstadoCivil = (EditText) findViewById(R.id.edit_estado_civil_usuario);
+        EditText etNumHijos = (EditText) findViewById(R.id.edit_hijos_usuario);
+        EditText etNumNietos = (EditText) findViewById(R.id.edit_nietos_usuario);
+
         btnCancelar.setOnClickListener(this);
     }
 
