244e4666fb2386b3eae5267697559da29d2718b9
iramD24 <idr.diaz96@gmail.com>
2017-05-01 20:42:07 -0500

    Detail Activities and Fragments
    Detail Activities and Fragments added

--- app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/DetalleHistoriaPersonalActivity.java
+++ app/src/main/java/itesm/mx/losalacranes_mipasadoenpresente/DetalleHistoriaPersonalActivity.java
@@ -3,13 +3,11 @@ package itesm.mx.losalacranes_mipasadoenpresente;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetalleFamilia extends AppCompatActivity {
+public class DetalleHistoriaPersonalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_familia);
+        setContentView(R.layout.activity_detalle_historia_personal);
     }
-
-
 }
